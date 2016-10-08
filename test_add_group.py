7f2cfcf570cf092e0f279e9e344b6ebe681ef9d5
Gargamellos <soplu@poczta.fm>
2016-10-08 16:01:54 +0200

    creating helper methods

--- test_add_group.py
+++ test_add_group.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.chrome.webdriver import WebDriver
-from selenium.webdriver.common.action_chains import ActionChains
-import time, unittest
-
+import unittest
 
 def is_alert_present(wd):
     try:
@@ -14,11 +12,12 @@ def is_alert_present(wd):
 class test_add_group(unittest.TestCase):
     def setUp(self):
         self.wd = WebDriver()
-    
-    def test_test_add_group(self):
-        success = True
-        wd = self.wd
+        self.wd.implicitly_wait(60)
+
+    def open_home_page(self, wd):
         wd.get("http://localhost:8080/addressbook/")
+
+    def login(self, wd):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
@@ -26,8 +25,14 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+
+    def open_group_page(self, wd):
         wd.find_element_by_link_text("grupy").click()
+
+    def create_group(self, wd):
+        # init group creation
         wd.find_element_by_name("new").click()
+        # fill group firm
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys("hgvjv")
@@ -37,11 +42,24 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys("vghvh")
+        # submit group creation
         wd.find_element_by_name("submit").click()
+
+    def return_to_groups_page(self, wd):
         wd.find_element_by_link_text("group page").click()
+
+    def logout(self, wd):
         wd.find_element_by_link_text("Wyloguj się").click()
-        self.assertTrue(success)
-    
+
+    def test_test_add_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd)
+        self.open_group_page(wd)
+        self.create_group(wd)
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+
     def tearDown(self):
         self.wd.quit()
 
