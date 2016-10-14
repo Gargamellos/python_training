feed28334b6af88ea635b7010f8eb6880d45485b
Gargamellos <soplu@poczta.fm>
2016-10-14 22:25:42 +0200

    CONTACT wyodrębnienie klasy pomocniczej SessionHelper, separate auxiliary class SessionHelper

--- test_contact/test_add_contact.py
+++ test_contact/test_add_contact.py
@@ -13,21 +13,21 @@ def app(request):
 
 
 def test_add_contact(app):
-    app.login(username = "admin", password = "secret")
+    app.session.login(username = "admin", password = "secret")
     app.create_contact(Contact(name = "asd", middlename = "\\9", surname = "asda", nick = "adsa", title = "swed",
                         company = "aswq", address = "asa", homephone = "\\9", mobilephone = "45654",
                          workphone = "\\9", fax = "\\9", email = "asdas", email2 = "\\9", email3 = "\\9",
                         homepage = "sdfsd", address2 = "gfd", phone2 = "gfd",
                         notes = "gfd"))
-    app.logout()
+    app.session.logout()
 
 
 def test_add_empty_contact(app):
-    app.login(username = "admin", password = "secret")
+    app.session.login(username = "admin", password = "secret")
     app.create_contact(Contact(name = "", middlename = "", surname = "", nick = "", title = "",
                         company = "", address = "", homephone = "", mobilephone = "",
                         workphone = "", fax = "", email = "", email2 = "", email3 = "",
                         homepage = "", address2 = "", phone2 = "",
                         notes = ""))
-    app.logout()
+    app.session.logout()
 
