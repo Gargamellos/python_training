314325eff5f1f5a91b1465037d312230ebe7c655
Gargamellos <soplu@poczta.fm>
2016-10-21 22:11:00 +0200

    Zwiększamy Inteligencję fixtury:walidacja, intellectual fixture (for all)

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -1,10 +1,8 @@
 from model.contact import Contact
 
 def test_edit_first_contact(app):
-    app.session.login(username = "admin", password = "secret")
     app.contact.edit_first_contact(Contact(name ="edit", middlename ="edit", surname ="edit", nick ="edit", title ="edit",
                                company = "edit", address = "edit", homephone = "edit", mobilephone = "edit",
                                workphone = "edit", fax = "edit", email = "edit", email2 = "edit", email3 = "edit",
                                homepage = "edit", address2 = "edit", phone2 = "edit",
                                notes = "edit"))
-    app.session.logout()
