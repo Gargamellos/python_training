f1f292ee2ddd98c65126559edda3dd2b5eba60ff
Gargamellos <soplu@poczta.fm>
2016-10-22 10:58:55 +0200

    Zwiększenie inteligencji login/logout, intellectual login

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,7 +5,6 @@ from model.group import Group
 def test_add_group(app):
     app.group.create(Group(name="hgvjv", header="hgvhgv", footer="vghvh"))
 
-
 def test_add_empty_group(app):
     app.group.create(Group(name="", header="", footer=""))
 
