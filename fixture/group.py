403e6a0639626a82a386a1e7d3642e13de7e4b86
Gargamellos <soplu@poczta.fm>
2016-10-28 17:19:05 +0200

    Revert "Ładowanie informacji z testowanej aplikacji (do grupy jeszcze)"
    This reverts commit d996b44d614daff2d5113ff74d20c37469404af9.

--- fixture/group.py
+++ fixture/group.py
@@ -98,7 +98,6 @@ class GroupHelper:
         for element in wd.find_elements_by_css_selector("span.group"):
             text = element.text
             id = element.find_element_by_name("selected[]").get_attribute("value")
-            groups.append(Group(name=text, id=id))
         return groups
 
 
