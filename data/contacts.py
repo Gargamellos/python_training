30d105cef1b42865042852d998ed4f4be1c2b9b7
Gargamellos <soplu@poczta.fm>
2016-11-12 11:57:52 +0100

    Jeszcze jeden sposób parametryzacji: dynamiczny lekcja 6.5

--- data/contacts.py
+++ data/contacts.py
@@ -1,10 +1,8 @@
 
 from model.contact import Contact
-import random
-import string
 
 
-constant = [
+testdata = [
     Contact(lastname="lastname1", firstname="firstname1", middlename="middlename1", nick="nick1", title="title1",
             company="company1", address="address1", homephone="homephone1", mobilephone="mobilephone1",
             workphone="workphone1", fax="fax1", email="email1", email2="email1", email3="email1", homepage="homepage1",
@@ -14,24 +12,3 @@ constant = [
             workphone="workphone2", fax="fax2", email="email2", email2="email2", email3="email2", homepage="homepage2",
             address2="address2", phone2="phone2", notes="notes2"),
 ]
-
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [Contact(lastname="", firstname="", middlename="", nick="", title="", company="", address="", homephone="",
-                    mobilephone="", workphone="", fax="", email="", email2="", email3="", homepage="", address2="",
-                    phone2="", notes="")] + [
-            Contact(lastname=random_string("lastname", 10), firstname=random_string("firstname", 10),
-                    middlename=random_string("middlename", 10), nick=random_string("nick", 10),
-                    title=random_string("title", 10), company=random_string("company", 10),
-                    address=random_string("address", 10), homephone=random_string("home", 10),
-                    mobilephone=random_string("mobile", 10), workphone=random_string("work", 10),
-                    fax=random_string("fax", 10), email=random_string("email", 10), email2=random_string("email2", 10),
-                    email3=random_string("email3", 10), homepage=random_string("homepage",10),
-                    address2=random_string("address2", 10), phone2=random_string("phone2", 10),
-                    notes=random_string("notes", 10))
-            for i in range(5)
-]
-
