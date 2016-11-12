54a827b8ca22377bbd99937ea19f552e38601ef3
Gargamellos <soplu@poczta.fm>
2016-11-12 14:12:06 +0100

    zmiany po zadaniu 18 do stanu zadania 19

--- data/contacts.py
+++ data/contacts.py
@@ -3,12 +3,12 @@ from model.contact import Contact
 
 
 testdata = [
-     Contact(lastname="lastname1", firstname="firstname1", middlename="middlename1", nick="nick1", title="title1",
-             company="company1", address="address1", homephone="homephone1", mobilephone="mobilephone1",
-             workphone="workphone1", fax="fax1", email="email1", email2="email1", email3="email1", homepage="homepage1",
-             address2="address1", phone2="phone1", notes="notes1"),
-     Contact(lastname="lastname2", firstname="firstname2", middlename="middlename2", nick="nick2", title="title2",
-             company="company2", address="address2", homephone="homephone2", mobilephone="mobilephone2",
-             workphone="workphone2", fax="fax2", email="email2", email2="email2", email3="email2", homepage="homepage2",
-             address2="address2", phone2="phone2", notes="notes2"),
+    Contact(lastname="lastname1", firstname="firstname1", middlename="middlename1", nick="nick1", title="title1",
+            company="company1", address="address1", homephone="homephone1", mobilephone="mobilephone1",
+            workphone="workphone1", fax="fax1", email="email1", email2="email1", email3="email1", homepage="homepage1",
+            address2="address1", phone2="phone1", notes="notes1"),
+    Contact(lastname="lastname2", firstname="firstname2", middlename="middlename2", nick="nick2", title="title2",
+            company="company2", address="address2", homephone="homephone2", mobilephone="mobilephone2",
+            workphone="workphone2", fax="fax2", email="email2", email2="email2", email3="email2", homepage="homepage2",
+            address2="address2", phone2="phone2", notes="notes2"),
 ]
--- data/groups.py
+++ data/groups.py
@@ -8,4 +8,3 @@ testdata = [
 ]
 
 
-
