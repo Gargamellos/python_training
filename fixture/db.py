864f3f219326a6a70531143f000645067a20dc4e
Gargamellos <soplu@poczta.fm>
2016-11-19 16:03:59 +0100

    Ex20 cd.. Assertions for groups and contacts

--- fixture/db.py
+++ fixture/db.py
@@ -29,8 +29,10 @@ class DbFixture:
         list = []
         cursor = self.connection.cursor()
         try:
+            cursor.execute("select id, firstname, lastname, address, home, mobile, work, email, email2, email3, phone2 from addressbook where deprecated='0000-00-00 00:00:00'")
             for row in cursor:
                 (id, firstname, lastname, address, home, mobile, work, email, email2, email3, phone2) = row
+                list.append(Contact(id=str(id), firstname=firstname, lastname=lastname, address=address, homephone=home,
                                   mobilephone=mobile, workphone=work, email=email, email2=email2, email3=email3, phone2=phone2))
         finally:
             cursor.close()
