ab86e6daed40510492326c948eebbd2681d04712
Gargamellos <soplu@poczta.fm>
2016-11-20 10:42:32 +0100

    Ex21 check contacts information between homepage and database

--- test/test_verification_contact.py
+++ test/test_verification_contact.py
@@ -2,16 +2,23 @@ from model.contact import Contact
 from random import randrange
 import re
 
-def test_verification_on_home_page(app):
-    old_contacts = app.contact.get_contact_list()
-    index = randrange(len(old_contacts))
-    contact_from_home_page = app.contact.get_contact_list()[index]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
-    assert contact_from_home_page.lastname == contact_from_edit_page.lastname
-    assert contact_from_home_page.address == contact_from_edit_page.address
-    assert contact_from_home_page.all_mails_from_home_page == merge_mails_like_on_home_page(contact_from_edit_page)
-    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
+def test_verification_on_home_page(app,db):
+    contact_from_home_page = app.contact.get_contact_list()
+    contact_from_db_list = db.get_contact_list()
+    assert sorted(contact_from_home_page, key=Contact.id_or_max) == sorted(contact_from_db_list, key=Contact.id_or_max)
+
+
+
+#def test_verification_on_home_page(app):
+#    old_contacts = app.contact.get_contact_list()
+#    index = randrange(len(old_contacts))
+#    contact_from_home_page = app.contact.get_contact_list()[index]
+#    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
+#    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
+#    assert contact_from_home_page.lastname == contact_from_edit_page.lastname
+#    assert contact_from_home_page.address == contact_from_edit_page.address
+#    assert contact_from_home_page.all_mails_from_home_page == merge_mails_like_on_home_page(contact_from_edit_page)
+#    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
 
 def clear(s):
     return re.sub("[() -]", "", s)
