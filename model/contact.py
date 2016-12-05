4af557b023c17e9bee0d64f3dce7a3546cc2ee78
Gargamellos <soplu@poczta.fm>
2016-12-05 22:06:33 +0100

    Ex 21 finish, end, over ...

--- model/contact.py
+++ model/contact.py
@@ -29,19 +29,14 @@ class Contact:
         self.id = id
 
     def __repr__(self):
-        return "%s:%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s" % (self.id, self.lastname, self.firstname,
+        return "%s:%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s" % (self.id, self.lastname, self.firstname,
                              self.middlename, self.nick, self.title, self.company, self.address,
                              self.homephone, self.mobilephone, self.workphone, self.fax, self.email, self.email2,
-                             self.email3, self.homepage, self.address2, self.phone2, self.notes,
-                             self.all_phones_from_home_page, self.all_mails_from_home_page)
+                             self.email3, self.homepage, self.address2, self.phone2, self.notes)
 
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id) and self.lastname == other.lastname\
-               and self.firstname == other.firstname, self.address == other.address, self.email == other.email,\
-               self.email2 == other.email2, self.email3 == other.email3, self.homephone == other.homephone,\
-               self.mobilephone == other.mobilephone, self.workphone == other.workphone, self.phone2 == other.phone2,\
-               self.all_mails_from_home_page == other.all_mails_from_home_page,\
-               self.all_phones_from_home_page == other.all_phones_from_home_page
+               and self.firstname == other.firstname
 
     def id_or_max(self):
         if self.id:
--- test/test_verification_contact.py
+++ test/test_verification_contact.py
@@ -4,32 +4,17 @@ import re
 
 
 def test_verification_on_home_page(app,db):
-    contact_from_home_page = app.contact.get_contact_list()
-    def clean(contact):
-        return Contact(id=contact.id, lastname=contact.lastname.strip(), firstname=contact.firstname.strip(),
-                           address=contact.address.strip(),homephone=contact.homephone.strip(),
-                           mobilephone=contact.mobilephone.strip(), workphone=contact.workphone.strip(),
-                           email=contact.email.strip(), email2=contact.email2.strip(),
-                           email3=contact.email3.strip(), phone2=contact.phone2.strip())
-    contact_from_db = map(clean,db.get_contact_list())
-    assert sorted(contact_from_home_page, key=Contact.id_or_max) == sorted(contact_from_db, key=Contact.id_or_max)
-
-
-#    assert contact_from_home_page.firstname == contact_from_db.firstname
-#    assert sorted(contact_from_home_page, key=Contact.id_or_max) == sorted(contact_from_db, key=Contact.id_or_max)
-#    assert contact_from_home_page.all_mails_from_home_page == merge_mails_like_on_home_page(contact_from_db)
-#    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_db)
-
-#def test_verification_on_home_page(app):
-#    old_contacts = app.contact.get_contact_list()
-#    index = randrange(len(old_contacts))
-#    contact_from_home_page = app.contact.get_contact_list()[index]
-#    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-#    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
-#    assert contact_from_home_page.lastname == contact_from_edit_page.lastname
-#    assert contact_from_home_page.address == contact_from_edit_page.address
-#    assert contact_from_home_page.all_mails_from_home_page == merge_mails_like_on_home_page(contact_from_edit_page)
-#    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
+    contact_from_home_page = sorted(app.contact.get_contact_list(),key=Contact.id_or_max)
+    contact_from_db = sorted(db.get_contact_list(),key=Contact.id_or_max)
+    assert len(contact_from_home_page) == len(contact_from_db)
+    for i in range (len(contact_from_db)):
+        db_contact = contact_from_db[i]
+        ui_contact = (sorted(contact_from_home_page, key=Contact.id_or_max))[i]
+        assert ui_contact.firstname == db_contact.firstname
+        assert ui_contact.lastname == db_contact.lastname
+        assert ui_contact.address == db_contact.address
+        assert ui_contact.all_mails_from_home_page == merge_mails_like_on_home_page(db_contact)
+        assert ui_contact.all_phones_from_home_page == merge_phones_like_on_home_page(db_contact)
 
 def clear(s):
     return re.sub("[() -]", "", s)
@@ -57,19 +42,5 @@ def merge_phones_like_on_home_page(db):
 #                                filter(lambda x: x is not None,
 #                                       [contact.homephone, contact.mobilephone, contact.workphone, contact.phone2]))))
 
-#def test_verification_on_home_page(app,db):
-#    contact_from_home_page = app.contact.get_contact_list()
-#    def clean(contact):
-#        return Contact(id=contact.id, lastname=contact.lastname.strip(), firstname=contact.firstname.strip(),
-#                       address=contact.address.strip(),homephone=contact.homephone.strip(),
-#                       mobilephone=contact.mobilephone.strip(), workphone=contact.workphone.strip(),
-#                       email=contact.email.strip(), email2=contact.email2.strip(),
-#                       email3=contact.email3.strip(), phone2=contact.phone2.strip())
-#    contact_from_db = map(clean,db.get_contact_list())
-#    assert len(contact_from_home_page) == len(contact_from_db)
-#    assert contact_from_home_page.firstname == contact_from_db.firstname
-#    assert sorted(contact_from_home_page, key=Contact.id_or_max) == sorted(contact_from_db, key=Contact.id_or_max)
-#    assert contact_from_home_page.all_mails_from_home_page == merge_mails_like_on_home_page(contact_from_db)
-#    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_db)
 
 
